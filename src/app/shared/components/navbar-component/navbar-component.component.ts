11287e7b0be5200359850ec88c13d8bf5aa4d0ea
Fernando Valle <fernando.valle@performait.com>
2022-05-26 11:37:19 -0300

    Create navbar component

--- src/app/shared/components/navbar-component/navbar-component.component.ts
+++ src/app/shared/components/navbar-component/navbar-component.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar-component',
+  selector: 'navbar-component',
   templateUrl: './navbar-component.component.html',
   styleUrls: ['./navbar-component.component.scss']
 })
 export class NavbarComponentComponent implements OnInit {
+  public routes: Route[] = [
+    { title: 'Home', url: '/home', active: true },
+    { title: 'Videos', url: '/videos', active: false },
+    { title: 'Channels', url: '/channels', active: false }
+  ];
+  public options: string[] = ["Subscriptions", "History", "Upload"]
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface Route {
+  title: string,
+  url: string,
+  active: boolean
+}
+
+interface Options {
+  title: string
 }

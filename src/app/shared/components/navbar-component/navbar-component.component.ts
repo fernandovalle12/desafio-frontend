cd657bf6ed3bdfe4119f7a4782692f723ba875f5
Fernando Valle <fernando.valle@performait.com>
2022-06-04 21:54:10 -0300

    Autocomplete

--- src/app/shared/components/navbar-component/navbar-component.component.ts
+++ src/app/shared/components/navbar-component/navbar-component.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 
 @Component({
     selector: 'navbar-component',
@@ -15,15 +17,23 @@ export class NavbarComponentComponent implements OnInit {
     ];
     public options: string[] = ["Subscriptions", "History", "Upload"];
     public filterFormGroup = new FormGroup({});
+    public searchKeys: string[] = [];
+    public flag: boolean = false;
+    public searchOptions: string[] = [];
 
+    private ngUnsubscribe = new Subject();
     constructor(
         private router: Router,
         private route: ActivatedRoute,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private localStorageService: LocalStorageService
     ) { }
 
     ngOnInit(): void {
         this.filterFormGroup = this.getFormGroup();
+        this.searchKeys = this.localStorageService.get('search');
+
+        this.valueChanges();
     }
 
     private getFormGroup() { 
@@ -39,12 +49,32 @@ export class NavbarComponentComponent implements OnInit {
 
     public onSearch() {
         let searchKey = this.filterFormGroup.get('search')?.value;
-        this.router.navigate([searchKey], { relativeTo: this.route })
+        this.searchKeys.push(searchKey);
+        this.localStorageService.set('search', this.searchKeys);
+        this.router.navigate([searchKey], { relativeTo: this.route });
     }   
 
     public onNavigate(route: Route) {
-        console.log(route);
         this.router.navigate([route.url], { relativeTo: this.route });
+    }
+
+    public onFocusOut() {
+        this.flag = false;
+    }
+
+    public onFocus() {
+        this.flag = true;
+        this.searchOptions = this.searchKeys;
+    }
+
+    public onSelect(key: string) {
+        this.filterFormGroup.get('search')?.patchValue(key);
+    }
+
+    private valueChanges() { 
+        this.filterFormGroup.get('search')?.valueChanges.pipe(takeUntil(this.ngUnsubscribe)).subscribe((val: string) => {
+            this.searchOptions = this.searchKeys.filter((key: string) => key.toLowerCase().includes(val.toLowerCase()));
+        });
     }
 
 }
